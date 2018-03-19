4cd60b58d7307d655fe173d401d54872159f0518
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-03-19 11:58:31 +0100

    met IndexController

--- src/main/java/be/vdab/frituurfrida/web/IndexController.java
+++ src/main/java/be/vdab/frituurfrida/web/IndexController.java
@@ -3,17 +3,16 @@ package be.vdab.frituurfrida.web;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 class IndexController {
+	@GetMapping
 	String index() {
 		DayOfWeek vandaag = LocalDate.now().getDayOfWeek();
-		if (vandaag == DayOfWeek.MONDAY || vandaag == DayOfWeek.THURSDAY) {
-			return "gesloten";
-		}
-		return "open";
+		return (vandaag == DayOfWeek.MONDAY || vandaag == DayOfWeek.THURSDAY) ? "gesloten" : "open";
 	}
 }
