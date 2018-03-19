02f2ca57cfeaac7ca54940e21fc217cf7cb7a409
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-03-19 13:38:45 +0100

    met indexController en jsp

--- src/main/java/be/vdab/frituurfrida/web/IndexController.java
+++ src/main/java/be/vdab/frituurfrida/web/IndexController.java
@@ -6,13 +6,15 @@ import java.time.LocalDate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/")
 class IndexController {
 	@GetMapping
-	String index() {
+	ModelAndView index() {
 		DayOfWeek vandaag = LocalDate.now().getDayOfWeek();
-		return (vandaag == DayOfWeek.MONDAY || vandaag == DayOfWeek.THURSDAY) ? "gesloten" : "open";
+		String openOfGesloten = (vandaag == DayOfWeek.MONDAY || vandaag == DayOfWeek.THURSDAY) ? "gesloten" : "open";
+		return new ModelAndView("index","openOfGesloten",openOfGesloten);
 	}
 }
