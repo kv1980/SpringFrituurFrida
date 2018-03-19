1dc9ad5bf1010474f6c17c029ed9a8e881bd5d06
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-03-19 10:54:16 +0100

    eerste static website

---
+++ src/test/java/be/vdab/frituurfrida/FrituurfridaApplicationTests.java
@@ -0,0 +1,16 @@
+package be.vdab.frituurfrida;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FrituurfridaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
