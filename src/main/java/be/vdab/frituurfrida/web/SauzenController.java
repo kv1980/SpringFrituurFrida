65fbeb6e3e19e1834d9d850be4131af4fe1392c5
Kristof <Kristof@192.168.0.129>
2018-04-12 19:45:01 +0200

    voor @Mock

--- src/main/java/be/vdab/frituurfrida/web/SauzenController.java
+++ src/main/java/be/vdab/frituurfrida/web/SauzenController.java
@@ -12,13 +12,6 @@ import be.vdab.frituurfrida.services.SausService;
 class SauzenController {
 	private final static String SAUZEN_JSP = "sauzen";
 	private final SausService sausService;
-/*	private final List<Saus> sauzen = Arrays.asList(
-			new Saus(1L,"mayonaise", Arrays.asList("olie","eidooier","citroensap","mosterd")),
-			new Saus(2L,"cocktail", Arrays.asList("mayonaise","room","ketchup","paprikapoeder","sherry","cognac")),
-			new Saus(3L,"mosterd", Arrays.asList("gemalen mosterdzaden","azijn","water")),
-			new Saus(4L,"tartare", Arrays.asList("mayonaise","ei","peterselie","sjalot","augurk","kappertjes","bieslook")),
-			new Saus(5L,"vinaigrette", Arrays.asList("olijfolie","wijnazijn","mosterd","honing","tuinkruiden"))
-			);*/
 	
 	SauzenController(SausService sausService){
 		this.sausService = sausService;
--- src/test/java/be/vdab/frituurfrida/services/DefaultSausServiceTest.java
+++ src/test/java/be/vdab/frituurfrida/services/DefaultSausServiceTest.java
@@ -1,7 +1,6 @@
 package be.vdab.frituurfrida.services;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,25 +15,25 @@ import be.vdab.frituurfrida.repositories.SausRepository;
 public class DefaultSausServiceTest {
 	private DefaultSausService service;
 	private SausRepository dummySausRepository;
+	private List<Saus> sauzenLijst = Arrays.asList(
+			new Saus(1L,"mayonaise", Arrays.asList("olie","eidooier","citroensap","mosterd")),
+			new Saus(2L,"cocktail", Arrays.asList("mayonaise","room","ketchup","paprikapoeder","sherry","cognac")),
+			new Saus(3L,"mosterd", Arrays.asList("gemalen mosterdzaden","azijn","water")),
+			new Saus(4L,"tartare", Arrays.asList("mayonaise","ei","peterselie","sjalot","augurk","kappertjes","bieslook")),
+			new Saus(5L,"vinaigrette", Arrays.asList("olijfolie","wijnazijn","mosterd","honing","tuinkruiden"))
+			);;
 	
 	@Before
 	public void before() {
 		dummySausRepository = Mockito.mock(SausRepository.class);
 		Mockito.when(dummySausRepository.findAll())
-			   .thenReturn(Arrays.asList(
-						new Saus(1L,"mayonaise", Arrays.asList("olie","eidooier","citroensap","mosterd")),
-						new Saus(2L,"cocktail", Arrays.asList("mayonaise","room","ketchup","paprikapoeder","sherry","cognac")),
-						new Saus(3L,"mosterd", Arrays.asList("gemalen mosterdzaden","azijn","water")),
-						new Saus(4L,"tartare", Arrays.asList("mayonaise","ei","peterselie","sjalot","augurk","kappertjes","bieslook")),
-						new Saus(5L,"vinaigrette", Arrays.asList("olijfolie","wijnazijn","mosterd","honing","tuinkruiden"))
-						));
+			   .thenReturn(sauzenLijst);
 		service = new DefaultSausService(dummySausRepository);
 	}
 	
 	@Test
-	public void serviceVindtEenSaus() {
-		Saus saus = new Saus(1L,"mayonaise", Arrays.asList("olie","eidooier","citroensap","mosterd"));
-		assertTrue(service.findAll().contains(saus));
+	public void serviceVindtDeLijstMetSauzen() {
+		assertEquals(sauzenLijst,service.findAll());
 		Mockito.verify(dummySausRepository).findAll();
 	}
 }
